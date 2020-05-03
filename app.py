ae833050afc909464f3a25cdb33cb9c539ac1b40
Ruthy Levi <rlevi@stevens.edu>
2020-05-02 22:16:12 -0400

    base template added for inheritance

--- app.py
+++ app.py
@@ -2,23 +2,27 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/MeetTheTeam')
 def team():
     return render_template("MeetTheTeam.html")
 
+
 @app.route('/quiz')
 def quiz():
     return render_template("quiz.html")
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
 
