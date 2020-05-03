69a0a20e8a60c55548808032ca076860ebc454c1
Ruthy Levi <rlevi@stevens.edu>
2020-05-03 16:22:03 -0400

    Added button that executes spotify generator :)

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, redirect, url_for, render_template
+import sys
+import spotipy
+import spotipy.util as util
 
 app = Flask(__name__)
 
@@ -18,8 +21,41 @@ def quiz():
     return render_template("quiz.html")
 
 
-@app.route('/playlistgenerator')
+@app.route('/playlistgenerator', methods=['GET', 'POST'])
 def playlistgenerator():
+    scope = 'user-library-read user-top-read playlist-read-private user-library-modify playlist-read-collaborative playlist-modify-public playlist-modify-private'
+    cid = 'd77d13817b264bacb100a71642df973e'
+    secret = '7d11f579e22e4b49867fdd8504fe78df'
+    redirect_uri = 'http://localhost:8888'
+    username = ""
+
+    if len(sys.argv) > 1:
+        username = sys.argv[1]  # take username as an argument
+    else:
+        print("Usage: %s username" % (sys.argv[0],))
+        # sys.exit()
+
+    # get user's permission
+    token = util.prompt_for_user_token(username, scope, cid, secret, redirect_uri)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        topArtists = sp.current_user_top_artists(limit=5)
+        artist_seeds = []
+        for artist in topArtists['items']:
+            artist_seeds.append(artist['id'])
+        id = sp.current_user()['id']
+        playlist_id = (sp.user_playlist_create(
+            id, "Quaransite Recommendations!", public=True))['id']
+        track_ids = []
+        rec = sp.recommendations(seed_artists=artist_seeds,
+                                seed_genres=None, seed_tracks=None, limit=20, country=None)
+        for track in rec['tracks']:
+            track_ids.append(track['id'])
+
+        sp.user_playlist_add_tracks(id, playlist_id, track_ids, position=None)
+    else:
+        print("Can't get token for", username)
+
     return render_template("playlistgen.html")
 
 
