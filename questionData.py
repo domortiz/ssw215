e289f357a3c8e7c782e0e7348496d62d9e306e86
Kenneth Skelton <kskelto1@stevens.edu>
2020-05-07 01:26:19 -0400

    whoops

--- questionData.py
+++ questionData.py
@@ -1,6 +1,6 @@
 quizOutcomes = ["Altered Carbon","Parks and Recreation","The Good Place","Tiger King","You","Black Mirror","The Witcher","Attack on Titan","Our Planet","Glee"]
 
-questionText = {1:"Do you like science fiction",2:"Do you like romance",""}
+questionText = {1:"Do you like science fiction",2:"Do you like romance",}
 
 questionChoices = {1:["Yes","No","maybe","so"],2:["Absolutely","Not on your life"]}
 
